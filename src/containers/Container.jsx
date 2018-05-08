284b8e78141c464abace382c3ec8c14c1292a0a6
ttd1997 <trieutandanh1997@gmail.com>
2018-05-08 13:33:24 +0700

    v1

---
+++ src/containers/Container.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Container extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Container;
